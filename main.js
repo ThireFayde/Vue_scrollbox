7baf463fb6bb7e39d0c32d753337a55f049ac874
原田 拓磨 <t.harada@april-knights.com>
2018-11-03 03:19:42 +0900

    ガジェットのスタイルを設定 スクロール量をボックス内の高さに反映させる

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 var app = new Vue({
     el: '#scroollBox',
     data: {
+        scrollY: 0,
+        timer: null,
+        styleObject: {
+            width: '100%',
+            position:'absolute',
+            background: '#fff',
+            opacity: '0.5',
+            height: '100px',
+        }
     },
     created: function(){
         window.addEventListener('scroll', this.handleScroll)
@@ -9,5 +18,9 @@ var app = new Vue({
         window.removeEventListener('scroll', this.handleScroll)
     },
     methods: {
+        handleScroll: function(){
+            this.scrollY = window.scrollY;
+            this.styleObject['top'] = this.scrollY+'px';
+        }
     }
 });
