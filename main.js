71488c737f77d8f6c4481b08f871aea8672da51f
原田 拓磨 <t.harada@april-knights.com>
2018-11-03 05:06:08 +0900

    topとheightを算出

--- main.js
+++ main.js
@@ -3,24 +3,45 @@ var app = new Vue({
     data: {
         scrollY: 0,
         timer: null,
+
+        berHeight: 0,
+        boxHeight: 400,
+        pageHeight: document.documentElement.scrollHeight || document.body.scrollHeight,
+        windowHeight: window.innerHeight,
+
         styleObject: {
             width: '100%',
             position:'absolute',
             background: '#fff',
             opacity: '0.5',
-            height: '100px',
         }
     },
     created: function(){
         window.addEventListener('scroll', this.handleScroll)
+        window.addEventListener('resize', this.handleResize)
+
+        //berの高さをDOM作成時に算出
+        this.windowHeight = window.innerHeight;
+        this.berHeight = this.windowHeight * this.boxHeight / this.pageHeight;
+        this.styleObject['height'] =  this.berHeight +'px';
+        
+        //berのheightを算出
+        this.scrollY =  this.boxHeight * window.scrollY / this.pageHeight;
+        this.styleObject['top'] = this.scrollY+'px';
     },
     beforeDestroy: function(){
         window.removeEventListener('scroll', this.handleScroll)
+        window.removeEventListener('resize', this.handleResize)
     },
     methods: {
         handleScroll: function(){
-            this.scrollY = window.scrollY;
+            this.scrollY =  this.boxHeight * window.scrollY / this.pageHeight;
             this.styleObject['top'] = this.scrollY+'px';
+        },
+        handleResize: function(){
+            this.windowHeight = window.innerHeight;
+            this.berHeight = this.windowHeight * this.boxHeight / this.pageHeight;
+            this.styleObject['height'] =  this.berHeight +'px';
         }
     }
 });
